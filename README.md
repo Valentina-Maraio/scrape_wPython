6b7f6666d969bfce985d643106bb603987b724ff
Valentina-Maraio <valentina.maraio89@gmail.com>
2024-05-15 10:23:44 +0200

    scarape wikipedia, get a json with authors' name, birth and death dates.

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 This project needs this version of urllib3
 pip install urllib3==1.25.10
+
+
+# **Code Explanation:**
+
+This code is a Python script that scrapes a Wikipedia page to extract information about English novelists.
+It uses libraries like pandas, requests, BeautifulSoup, time, and re for web scraping, data manipulation, and handling regular expressions.
+The script sends an HTTP GET request to the Wikipedia page, parses the HTML content using BeautifulSoup, and then extracts relevant information about authors from the page.
+It extracts author names, birth dates, and death dates (if available) from the HTML code.
+The extracted data is then stored in a list of dictionaries where each dictionary represents an author's information.
+The script then sorts the author names alphabetically, creates a pandas DataFrame from the extracted data, adds a delay to avoid overwhelming the website with requests, and finally exports the data to a JSON file named authors_names_1900s.json.
+
+# **Documentation:**
+### Purpose:
+This script scrapes Wikipedia for a list of English novelists and their birth/death dates.
+
+## Libraries Used:
+pandas: For data manipulation and creating a DataFrame.
--- authors_timeline_project/authors.py
+++ authors_timeline_project/authors.py
@@ -1,9 +1,9 @@
 # scrape the wikipedia for Authors' names
 import pandas as pd
 import requests
-
 from bs4 import BeautifulSoup
 import time
+import re
 
 # URL of the website to scrape
 url = "https://en.wikipedia.org/wiki/List_of_English_novelists"
@@ -18,17 +18,28 @@ soup = BeautifulSoup(res.content, 'html.parser')
 authors = []
 for ul in soup.select('div.div-col ul'):
     for li in ul.find_all('li'):
+        # Extract the data in the double quotes
+        text = li.get_text(strip=True)
+        date = re.search(r'\((.*?)\)', text).group(1) if re.search(r'\((.*?)\)', text) else None
         name = li.find('a').get_text()
-        authors.append(name)
+
+        if date and '\u2013' in date:
+            birth, *death = date.split('\u2013', 1)
+            death = '\u2013'.join(death)
+        else:
+            birth = date.strip() if date else None
+            death = None
+
+        authors.append({'author_name': name, 'birth': birth, 'death': death})
 
 # Sort the authors' names alphabetically
 # authors.sort()
 
 # Store the information in a pandas dataframe
-df = pd.DataFrame(authors, columns=['Name'])
+df = pd.DataFrame(authors)
 
 # Add a delay between requests to avoid overwhelming the website with requests
 time.sleep(1)
 
 # Export the data to a CSV file
-df.to_json('authors_names_1900s.json', index=False)
+df.to_json('authors_names_1900s.json', orient='records', index=False)
